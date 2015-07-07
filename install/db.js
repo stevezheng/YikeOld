8283255854be76f6c60769cdeed6ae7522410746
stevezheng <stevezheng1989@gmail.com>
2015-07-07 19:41:03 +0800

    新增 install数据包-Item

--- install/db.js
+++ install/db.js
@@ -15,5 +15,32 @@ module.exports = {
     , image: ''
     , price: 0
     , sort: 255
+  },
+  Item: {
+    salenum: 0
+    , commentStar: 5
+    , originPrice: 0
+    , freight: 0
+    , name: '测试商品'
+    , shopId: '559bb388e4b023682f13d0f9'
+    , shopName: '测试店铺'
+    , marketPrice: 0
+    , categoryId: '559a156ae4b0001a92d9433a'
+    , categoryName: '美食'
+    , spec: '美食就是好好吃'
+    , serial: ''
+    , click: 0
+    , brandId: ''
+    , vat: 0
+    , collect: 0
+    , status: 1
+    , commentCount: 1
+    , comment: ''
+    , storage: 0
+    , image: 'http://ac-ry3vkr5q.clouddn.com/0hURloywo0W7Kls4dizIW8voMwzFTsOPm0PoQxtj.jpg'
+    , brandName: ''
+    , ad: ''
+    , transportId: ''
+    , price: 199
   }
 };
